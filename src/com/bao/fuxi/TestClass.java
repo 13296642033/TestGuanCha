7d819c25b4e4d76e35d756cd3dcd3f312b0069fd
胡炎帝 <yandihu@irissz.com>
2019-02-12 15:20:03 +0800

    test提交代码

--- src/com/bao/fuxi/TestClass.java
+++ src/com/bao/fuxi/TestClass.java
@@ -2,6 +2,7 @@ package com.bao.fuxi;
 
 public class TestClass {
 	public static void main(String[] args) {
+		// ²âÊÔgitÌá½»´úÂë
 		Maz m = new Maz();
 		PeopleObs p = new PeopleObs("ÍÛ¹ş¹ş");
 		PeopleObs p2 = new PeopleObs("Ë¬ÍáÍá");
